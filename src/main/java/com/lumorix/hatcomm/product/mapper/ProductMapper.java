e58a67811b70441aec4a82c03415da3a048ce4b7
Saket Kumar <mac2me.saket@gmail.com>
2026-06-16 10:54:46 +0530

    correcting the mapper code

--- src/main/java/com/lumorix/hatcomm/product/mapper/ProductMapper.java
+++ src/main/java/com/lumorix/hatcomm/product/mapper/ProductMapper.java
@@ -1,6 +1,7 @@
 package com.lumorix.hatcomm.product.mapper;
 
 import com.lumorix.hatcomm.product.dto.CreateProductRequest;
+import com.lumorix.hatcomm.product.dto.ProductResponse;
 import com.lumorix.hatcomm.product.entity.ProductEntity;
 import com.lumorix.hatcomm.product.enumeration.ProductStatus;
 import org.springframework.stereotype.Component;
@@ -8,11 +9,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProductMapper {
 
-    public ProductEntity toResponse(CreateProductRequest productRequest){
-        ProductEntity product = new ProductEntity();
-        product.setDescription(productRequest.description());
-        product.setName(productRequest.name());
-        product.setPrice(productRequest.price());
-        return product;
+    public ProductResponse toResponse(ProductEntity productEntity){
+        return new ProductResponse(
+                productEntity.getProductId(),
+                productEntity.getName(),
+                productEntity.getDescription(),
+                productEntity.getStatus().name(),
+                productEntity.getPrice()
+        );
     }
 }
